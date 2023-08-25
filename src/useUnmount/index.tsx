2744760d3c4ff23ac34e744651e221f210ab3cef
Batuhan Ozen <batuhan@simplisales.com>
2023-08-26 02:15:12 +0300

    created useUnmount hook

---
+++ src/useUnmount/index.tsx
@@ -0,0 +1,10 @@
+import { useEffect } from 'react'
+
+const useUnmount = (callback:()=>(void)) => {
+    useEffect(()=>{
+        return callback()
+    },[]);
+    return
+}
+
+export default useUnmount
