6c8ca34465443f004416d348e8dab8a64e6eb911
Batuhan Ozen <batuhan@simplisales.com>
2023-08-28 00:59:16 +0300

    update exports for all hooks

--- index.tsx
+++ index.tsx
@@ -1,3 +1,5 @@
 export { default as useDebounce } from './hooks/useDebounce';
-export { default as useUnmount } from './hooks/useUnmount';
+export { default as useEventListener } from './hooks/useEventListener';
 export { default as useHistoricalState } from './hooks/useHistoricalState';
+export { default as useOutsideClick } from './hooks/useOutsideClick';
+export { default as useUnmount } from './hooks/useUnmount';
